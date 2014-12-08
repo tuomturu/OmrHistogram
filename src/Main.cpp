b65ec21aba2c24a3fc57887a6448e4f0ffa0fe05
tuomas <tuomas.turunen@iki.fi>
2014-12-08 14:06:48 +0200

    initial

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 8, 2014
+ *      Author: tuomas
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	std::cout << "Moi maailma" << std::endl;
+
+	return EXIT_SUCCESS;
+}
